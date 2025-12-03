0c3e7443ac71dad31458b3312bf50e7b7d02bbe8
Finpama <phma020@gmail.com>
2025-12-03 11:12:16 -0300

    token request

--- api.py
+++ api.py
@@ -1,8 +1,61 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+import asyncio
+import requests as req
+import json
 
+from datetime import datetime
+
+lastRefresh = datetime.now()
+refreshDelay = 60 * 2 #Em segundos
+
+async def requestToken() -> str:
+    with open('./cmg/password.json') as pw:
+        jsonPW = json.loads(pw.read())
+
+    response = req.post('https://purcell.terminalonline.com.br/authenticate', json=jsonPW)
+
+    data = json.loads(response.content)
+    data_results = data.get('results')
+    TOKEN = data_results.get('token')
+    
+    return TOKEN
+
+async def refresh():
+    global lastRefresh
+    lastRefresh = datetime.now()
+    
+    TOKEN = await requestToken()
+    
+    print(f"     [refresh]: {lastRefresh}")
+    print(TOKEN)
+    
+async def run_refresh():
+    while True:
+        await refresh()
+        await asyncio.sleep(refreshDelay)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup:
+    print('     [lifespan]: running startup')
+    asyncio.create_task(run_refresh())
+    
+    yield
+    # shutdown:
+    print('     [lifespan]: running shutdown')
+    
+    
+
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
-async def root():
-    return "Hello World!"
+def read_status():
+    return {"last_refresh": lastRefresh}
+
+@app.get("/refresh")
+async def re():
+    await refresh()
+    return {"new_re": lastRefresh}
